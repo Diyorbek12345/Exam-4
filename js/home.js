e5921c87156053233058145567759fa247a776b1
diyorbek1234567 <yusupovamatluba>
2023-08-08 12:48:51 +0500

    structure has edited

--- js/home.js
+++ js/home.js
@@ -1,7 +1,79 @@
-const homeyMenuToggleBtn = document.querySelector(".wrapper button");
-const homeMenuDropdown = document.querySelector("#header .dropdown");
+const productcards = document.querySelectorAll(".cards");
+// console.log(productCard);
+function getProductCard(product) {
+  const productCard = document.createElement("div");
+  productCard.className = "product-card";
 
+  const productCardBody = document.createElement("div");
+  productCardBody.className = "product-card-body";
 
-homeyMenuToggleBtn.addEventListener("click", () => {
-  homeMenuDropdown.classList.toggle("hidden");
+  const productImg = document.createElement("img");
+  productImg.src = product.images[0];
+  productImg.alt = product.name;
+  productImg.className = "card1__img";
+  productImg.style = `
+  margin-bottom: 20px;
+  `
+
+  productCardBody.appendChild(productImg);
+
+  const productCardFooter = document.createElement("div");
+  productCardFooter.className = "product-card-footer";
+
+  const productTitle = document.createElement("h3");
+  const productTitleText = document.createTextNode(product.name);
+
+  productTitle.appendChild(productTitleText);
+
+  const productName = document.createElement("p");
+  productName.innerHTML =  `${product.name}`;
+
+  const productPrice = document.createElement("p");
+  productPrice.innerHTML = `${product.price}`;
+  productPrice.className = "cards_current__cost";
+  productPrice.style = `
+  margin-bottom: 10px;
+  `
+
+  const productBtn = document.createElement("a");
+  productBtn.innerHTML = "В корзину";
+  productBtn.className = "cards_btn"
+
+  productCardFooter.prepend(productBtn);
+  productCardFooter.prepend(productPrice);
+  productCardFooter.prepend(productTitle);
+
+  productCard.append(productCardBody, productCardFooter);
+  console.log(productCard);
+  return productCard;
+}
+
+products.slice(3, 7).map((product) => {
+  let card = getProductCard(product);
+  let card__copy1 = card.cloneNode(true);
+  let card__copy2 = card.cloneNode(true);
+  let card__copy3 = card.cloneNode(true);
+  productcards[0].append(card__copy1);
+  productcards[1].append(card__copy2);
+  productcards[2].append(card__copy3);
 });
+
+
+
+let articles = [
+  {
+    title: "qalesiz",
+    description: "oka nima gap",
+    image: "../assets/images/articles/1.png"
+  },
+  {
+    title: "qalesiz",
+    description: "oka nima gap",
+    image: "../assets/images/articles/2.png"
+  },
+  {
+    title: "qalesiz",
+    description: "oka nima gap",
+    image: "../assets/images/articles/3.png"
+  }
+]
--- js/main.js
+++ js/main.js
@@ -5,3 +5,30 @@ const homeMenuDropdown = document.querySelector("#header .dropdown");
 homeyMenuToggleBtn.addEventListener("click", () => {
   homeMenuDropdown.classList.toggle("hidden");
 });
+
+const TabButtons = document.querySelectorAll(".map__btns a")
+const TabContants = document.querySelectorAll(".btn__content")
+
+let active = 0;
+
+function getTabContent() {
+  TabContants.forEach((el, i) => {
+   if (i !== active) {
+    el.style = `
+    display: none;
+    `
+   } else{
+    el.style = `
+    display: inline-block;
+    `
+   }
+  })
+}
+
+getTabContent()
+
+TabButtons.forEach((el, i) => {
+  el.addEventListener("click", function () {
+    active = i
+  })
+})
